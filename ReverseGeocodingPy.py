a6dadf3fc94ff450fa635b0a0b9e7a36b383235b
Edoardo de Cal <edoardo.decal@gmail.com>
2020-01-16 16:58:00 +0100

    imported code

--- ReverseGeocodingPy.py
+++ ReverseGeocodingPy.py
@@ -1,1 +1,7 @@
-from pygeocoder import geocoder
+import reverse_geocoder as rg
+
+coordinates = (51.5214588,-0.1729636),(9.936033, 76.259952),(37.38605,-122.08385)
+
+results = rg.search(coordinates) # default mode = 2
+
+print results
